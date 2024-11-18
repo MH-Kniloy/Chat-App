39311e9ebe888db2d8311852d255d5405954f8aa
MH-Kniloy <web.kniloy@gmail.com>
2024-11-19 02:09:04 +0600

    responsive 768px

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -5,36 +5,36 @@ import registrationImg from '../../assets/registration.png'
 const Registration = () => {
   return (
     <>
-      <section className="flex">
-        <div className="w-1/2 pt-[225px] ps-[190px]">
-          <h1 className="text-darkBlueOne font-nunito text-[34px] font-bold mb-3">
+      <section className="md:flex">
+        <div className="md:w-1/2 md:pt-[225px] md:ps-[190px] pt-10 ps-4 pe-4 md:pe-0">
+          <h1 className="text-darkBlueOne font-nunito md:text-[34px] text-2xl font-bold md:mb-3 mb-2">
             Get started with easily register
           </h1>
 
-          <p className="text-xl opacity-50 font-nunito mb-[60px]">
+          <p className="md:text-xl text-[18px] opacity-50 font-nunito md:mb-[60px] mb-8">
             Free register and you can enjoy it
           </p>
 
           <form className="mb-[36px]">
-            <div className="form-control relative mb-[56px]">
+            <div className="form-control relative md:mb-[56px] mb-8">
               <input
-                className="peer w-[370px] pl-[53px] py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none bg-white"
-                type="email"
+                className="peer w-full md:w-[370px] md:pl-[53px] pl-8 py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none bg-white"
+                type="text"
                 required
               />
               <Wavelabel text={"Emailaddress"}></Wavelabel>
             </div>
-            <div className="form-control relative mb-[56px]">
+            <div className="form-control relative md:mb-[56px] mb-8">
               <input
-                className="peer w-[370px] pl-[53px] py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none"
+                className="peer w-full md:w-[370px] md:pl-[53px] pl-8 py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none bg-white"
                 type="text"
                 required
               />
               <Wavelabel text={"Fullname"}></Wavelabel>
             </div>
-            <div className="form-control relative mb-[56px]">
+            <div className="form-control relative md:mb-[56px] mb-8">
               <input
-                className="peer w-[370px] pl-[53px] py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none"
+                className="peer w-full md:w-[370px] md:pl-[53px] pl-8 py-[27px] text-darkBlueOne font-nunito text-xl font-semibold border-darkBlueOne border-[2px] border-opacity-30 rounded-[9px] block focus:outline-none bg-white"
                 type="password"
                 required
               />
@@ -46,15 +46,18 @@ const Registration = () => {
               value="Sign up"
             />
           </form>
-          <p className="text-darkBlueTwo font-opnesans text-[14px] text-center w-[350px]">
+          <p className="text-darkBlueTwo font-opnesans text-[14px] text-center w-[350px] mb-12 md:mb-0">
             Already have an account ?{" "}
-            <a className="text-orange font-opnesans text-[14px] font-bold" href="#">
+            <a
+              className="text-orange font-opnesans text-[14px] font-bold"
+              href="#"
+            >
               Sign In
             </a>
           </p>
         </div>
 
-        <div className="w-1/2">
+        <div className="md:w-1/2">
           <img
             className="w-full h-screen object-cover"
             src={registrationImg}
