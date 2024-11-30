b6ac83e86ef7545653740b0f5534e7cb939206ea
MH-Kniloy <web.kniloy@gmail.com>
2024-11-30 23:03:14 +0600

    firebase added

---
+++ src/components/Authentication/firebase.config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbfjT68dfdiN8hJyrRWxPXphe2kOD2BK8",
+  authDomain: "chatapp-b11c2.firebaseapp.com",
+  projectId: "chatapp-b11c2",
+  storageBucket: "chatapp-b11c2.firebasestorage.app",
+  messagingSenderId: "67648412239",
+  appId: "1:67648412239:web:4eaa79b95f916b0bbfe83a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
