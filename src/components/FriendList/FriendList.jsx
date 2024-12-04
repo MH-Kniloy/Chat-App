5dc854eec1e0f1437128a37ac60679d25b872b92
MH-Kniloy <web.kniloy@gmail.com>
2024-12-04 22:28:02 +0600

    FriendList, FriendListComp created

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { BsThreeDotsVertical } from "react-icons/bs";
+import friendlist1 from "../../assets/friend-list1.png";
+import friendlist2 from "../../assets/friend-list2.png";
+import friendlist3 from "../../assets/friend-list3.png";
+import friendlist4 from "../../assets/friend-list4.png";
+import FriendListComp from '../FriendListComp/FriendListComp';
+
+const FriendList = () => {
+  const friendDetails = [
+    {
+      image: `${friendlist1}`,
+      name: "Raghav",
+      message: "Dinner?",
+      time: "Today, 8:56pm",
+    },
+    {
+      image: `${friendlist2}`,
+      name: "Swathi",
+      message: "Sure!",
+      time: "Today, 2:31pm",
+    },
+    {
+      image: `${friendlist3}`,
+      name: "Kiran",
+      message: "Hi.....",
+      time: "Yesterday, 6:22pm",
+    },
+    {
+      image: `${friendlist4}`,
+      name: "Tejeshwini C",
+      message: "I will call him today.",
+      time: "Today, 12:22pm",
+    },
+    
+  ];
+  return (
+    <div className="p-5 rounded-[20px] shadow-custom">
+      <div className="flex justify-between mb-4">
+        <h3 className="font-poppins font-semibold text-xl">Friends</h3>
+        <BsThreeDotsVertical className="text-2xl text-violet cursor-pointer " />
+      </div>
+
+      <div>
+        {friendDetails.map((items) => (
+          <FriendListComp
+            image={items.image}
+            name={items.name}
+            message={items.message}
+            time={items.time}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FriendList
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Container from '../../components/Container/Container'
 import SidebarMenu from '../../components/SidebarMenu/SidebarMenu'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import GroupList from '../../components/GroupList/GroupList'
+import FriendList from '../../components/FriendList/FriendList'
 
 const Home = () => {
   return (
@@ -15,14 +16,15 @@ const Home = () => {
             <SidebarMenu />
           </div>
           
-          <div className='w-[90%] flex gap-4'>
+          <div className='w-[90%] flex gap-5'>
 
              <div className='w-[33%] '>
               <SearchBar />
               <GroupList />
              </div>
              <div className='w-[33%]'>
-              <SearchBar />
+              
+              <FriendList />
              </div>
              <div className='w-[33%]'>
               <SearchBar />
