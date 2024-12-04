19c545d1c3b1a3895eb203a18ff1606a2689662a
MH-Kniloy <web.kniloy@gmail.com>
2024-12-05 01:06:19 +0600

    pop up modal added onclicking logout

--- src/components/SidebarMenu/SidebarMenu.jsx
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -5,10 +5,12 @@ import { FiLogOut } from "react-icons/fi";
 import { AiOutlineMessage } from "react-icons/ai";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import Popup from "reactjs-popup";
 
 
 const SidebarMenu = () => {
+  const navigate = useNavigate()
   return (
     <div className="w-[10%] bg-violet h-[960px] rounded-[20px] flex justify-center me-[45px]">
       <nav className="flex flex-col items-center gap-[100px]">
@@ -67,9 +69,51 @@ const SidebarMenu = () => {
           </li>
         </ul>
         <div className="mt-0">
-          <Link to="/Login">
-            <FiLogOut className=" text-white text-[50px]  cursor-pointer" />
-          </Link>
+          <Link to="/Login"></Link>
+          <Popup
+            trigger={
+              <button className="button">
+                <FiLogOut className=" text-white text-[50px]  cursor-pointer" />
+              </button>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal bg-white shadow-custom bg-opacity-70 p-12 rounded-[10px]">
+                <div className="header font-poppins font-bold ">
+                  <p className="text-2xl">
+
+                  Logout
+                  </p>
+                </div>
+                <div className="content">
+                  <p className="font-poppins font-medium text-xl">
+                    Are you sure you want to logout?
+                  </p>
+                </div>
+                <div className="actions">
+                  <button
+                    className="button bg-violet px-5 py-2 rounded-[10px] text-xl font-semibold font-poppins text-white me-4"
+                    onClick={() => {
+                      navigate("/Login");
+                    }}
+                  >
+                    Yes
+                  </button>
+                  <button
+                    className="button bg-violet px-5 py-2 rounded-[10px] text-xl font-semibold font-poppins text-white"
+                    onClick={() => {
+                      console.log("modal closed ");
+                      close();
+                    }}
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
       </nav>
     </div>
