d488f6b5962aaf3372ca2cf3fbbdb89560af3970
MH-Kniloy <web.kniloy@gmail.com>
2024-12-11 22:49:36 +0600

    Redux store created

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { UserContext } from './context/UserContext/UserContext.jsx'
 import { GroupContext } from './context/GroupContext/GroupContext.jsx'
+import { Provider } from 'react-redux'
+import store from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <UserContext>
     <GroupContext>
-
     <App />
     </GroupContext>
     </UserContext>
+    </Provider>
   </StrictMode>,
 )
