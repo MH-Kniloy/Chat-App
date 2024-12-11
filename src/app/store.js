cab5a29cacb4022e2e06c35c33b29d9e21fa8069
MH-Kniloy <web.kniloy@gmail.com>
2024-12-11 23:55:21 +0600

    userSlice updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from '../features/User/userSlice'
+import userSlice from "../features/User/userSlice";
+
 
 export default configureStore({
   reducer: {
--- src/features/User/userSlice.js
+++ src/features/User/userSlice.js
@@ -1,4 +1,4 @@
-import  createSlice from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
   name: "userInfo",
@@ -6,11 +6,19 @@ export const userSlice = createSlice({
     value: 0,
   },
   reducers: {
-   
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    setValue: (state, action) => {
+      state.value = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { increment, decrement, setValue } = userSlice.actions;
 
 export default userSlice.reducer;
