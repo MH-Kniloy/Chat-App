53a4250f92d7e6a47116f9323c2262ff35fcfc7d
MH-Kniloy <web.kniloy@gmail.com>
2024-12-13 22:31:42 +0600

    user authentication added on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Container from '../../components/Container/Container'
 import SidebarMenu from '../../components/SidebarMenu/SidebarMenu'
 import SearchBar from '../../components/SearchBar/SearchBar'
@@ -8,8 +8,18 @@ import UserList from '../../components/UserList/UserList'
 import FriendRequest from '../../components/FriendRequest/FriendRequest'
 import MyGroup from '../../components/MyGroup/MyGroup'
 import BlockedUsers from '../../components/BlockedUsers/BlockedUsers'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const reduxData = useSelector(state=> state.userDetails.userCredentials)
+  const navigate= useNavigate()
+  useEffect(()=>{
+
+    if(!reduxData){
+         navigate("/Login")
+    }
+  })
   return (
     <>
     <Container>
