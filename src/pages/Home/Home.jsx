6f0e675a768882eb81d27e525a7187c8d0ca816f
MH-Kniloy <web.kniloy@gmail.com>
2024-12-14 04:35:41 +0600

    user authentication added on Notifications page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,66 +1,55 @@
-import React, { useEffect } from 'react'
-import Container from '../../components/Container/Container'
-import SidebarMenu from '../../components/SidebarMenu/SidebarMenu'
-import SearchBar from '../../components/SearchBar/SearchBar'
-import GroupList from '../../components/GroupList/GroupList'
-import FriendList from '../../components/FriendList/FriendList'
-import UserList from '../../components/UserList/UserList'
-import FriendRequest from '../../components/FriendRequest/FriendRequest'
-import MyGroup from '../../components/MyGroup/MyGroup'
-import BlockedUsers from '../../components/BlockedUsers/BlockedUsers'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Container from "../../components/Container/Container";
+import SidebarMenu from "../../components/SidebarMenu/SidebarMenu";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import GroupList from "../../components/GroupList/GroupList";
+import FriendList from "../../components/FriendList/FriendList";
+import UserList from "../../components/UserList/UserList";
+import FriendRequest from "../../components/FriendRequest/FriendRequest";
+import MyGroup from "../../components/MyGroup/MyGroup";
+import BlockedUsers from "../../components/BlockedUsers/BlockedUsers";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const reduxData = useSelector(state=> state.userDetails.userCredentials)
-  const navigate= useNavigate()
-  useEffect(()=>{
-
-    if(!reduxData){
-         navigate("/Login")
+  const reduxData = useSelector((state) => state.userDetails.userCredentials);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!reduxData) {
+      navigate("/Login");
     }
-  })
+  },[]);
   return (
     <>
-    <Container>
+      <Container>
+        <section className="flex">
+          <SidebarMenu />
 
-         <section className='flex'>
-            
-            <SidebarMenu />
-          
-          
-          <div className='w-[90%] flex gap-5 flex-wrap'>
-
-             <div className='w-[32%] '>
+          <div className="w-[90%] flex gap-5 flex-wrap">
+            <div className="w-[32%] ">
               <SearchBar />
               <GroupList />
-             </div>
-             <div className='w-[32%]'>
-              
+            </div>
+            <div className="w-[32%]">
               <FriendList />
-             </div>
-             <div className='w-[32%]'>
+            </div>
+            <div className="w-[32%]">
               <UserList />
-             </div>
-             <div className='w-[32%] '>
+            </div>
+            <div className="w-[32%] ">
               <FriendRequest />
-             </div>
-             <div className='w-[32%]'>
-              
+            </div>
+            <div className="w-[32%]">
               <MyGroup />
-             </div>
-             <div className='w-[32%]'>
+            </div>
+            <div className="w-[32%]">
               <BlockedUsers />
-             </div>
-
+            </div>
           </div>
-         
-         </section>
-
-    </Container>
-
+        </section>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
