7582edd60d4942397724855fe7849163de57244a
MH-Kniloy <web.kniloy@gmail.com>
2024-12-26 01:57:55 +0600

    all users shown in the userlist

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,9 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import UserListComp from '../UserListComp/UserListComp';
-import { userInfo } from '../../context/UserContext/UserContext';
+import UserListComp from "../UserListComp/UserListComp";
+import { userInfo } from "../../context/UserContext/UserContext";
+import { getDatabase, ref, onValue } from "firebase/database";
 const UserList = () => {
-  const userDetails = useContext(userInfo)
+  const userDetails = useContext(userInfo);
+  const db = getDatabase();
+  const dataRef = ref(db, "users/");
+  const [data, setData] = useState([])
+  console.log(data)
+  useEffect(()=>{
+    onValue(dataRef, (snapshot) => {
+      const data = snapshot.val();
+      const dataArr = Object.values(data);
+      setData(dataArr);
+    });
+  },[])
+
   return (
     <div className="p-5 pt-0 rounded-[20px] shadow-custom h-[450px] overflow-auto relative ">
       <div className="flex justify-between mb-3 pt-5 bg-white sticky top-[0px] left-0 h-[70px] w-full">
@@ -12,18 +25,33 @@ const UserList = () => {
       </div>
 
       <div>
-        {userDetails.map((items, idx) => (
+        {/* {userDetails.map((items, idx) => (
           <UserListComp
             key={idx}
             image={items.image}
             name={items.name}
             time={items.time}
           />
+        ))} */}
+        {/* {
+        onValue(dataRef, (snapshot) => {
+          const data = snapshot.val()
+          const dataArr = Object.values(data)
+          console.log(dataArr)
+          
+          
+        })
+        } */}
+        {data.map((items, idx)=>(
+           <UserListComp
+             key={idx}
+             image={items.profile_picture}
+             name={items.username}
+           />
         ))}
       </div>
     </div>
   );
-}
+};
 
-
-export default UserList
+export default UserList;
--- src/components/UserListComp/UserListComp.jsx
+++ src/components/UserListComp/UserListComp.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import profile_pic from "../../assets/profile-pic.png"
 
-const UserListComp = ({ image, name, time }) => {
+const UserListComp = ({ image, name, }) => {
   return (
     <div className="pe-3 pb-4 mb-4 border-b-[1px] border-gray-400 border-opacity-80 flex gap-4 items-center last:border-none">
-      <div>
-        <img src={image} alt="" />
+      <div className="rounded-full overflow-hidden w-[55px]">
+        <img src={image ? image : profile_pic} alt="" />
       </div>
       <div className="flex w-full justify-between">
         <div>
           <h4 className="font-poppins text-[18px] font-semibold">{name}</h4>
           <h5 className="text-[14px] font-poppins font-medium text-[#4D4D4DBF]">
-            {time}
+            Today, 8:56pm
           </h5>
         </div>
         <div className="text-center">
