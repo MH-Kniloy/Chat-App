d0db9abb14cdd53187fcf07ab5c88ad6c950b657
MH-Kniloy <web.kniloy@gmail.com>
2024-12-26 00:02:36 +0600

    .env.local file added and firebase apikeys changed

--- src/components/Authentication/firebase.config.jsx
+++ src/components/Authentication/firebase.config.jsx
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDiyTdUlc5UzvAnh4S4bH6RF7SH4t4X_vI",
-  authDomain: "chat-app-921fe.firebaseapp.com",
-  projectId: "chat-app-921fe",
-  storageBucket: "chat-app-921fe.firebasestorage.app",
-  messagingSenderId: "705119126418",
-  appId: "1:705119126418:web:26ef7bb6a51a3eef8ff74b",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
