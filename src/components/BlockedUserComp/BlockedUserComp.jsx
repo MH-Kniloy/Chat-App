e60bd7c2fc07b74171cf6ce44b6ea9de01b390e3
MH-Kniloy <web.kniloy@gmail.com>
2025-01-05 03:06:21 +0600

    blocked user appears on blockLst

--- src/components/BlockedUserComp/BlockedUserComp.jsx
+++ src/components/BlockedUserComp/BlockedUserComp.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import profile_pic from "../../assets/profile-pic.png";
 
-const BlockedUserComp = ({ image, name, time }) => {
+const BlockedUserComp = ({ items, blockedName, blockedPhoto, }) => {
+  
   return (
     <div className="pe-3 pb-4 mb-4 border-b-[1px] border-gray-400 border-opacity-80 flex gap-4 items-center last:border-none">
-      <div>
-        <img src={image} alt="" />
+      <div className="rounded-full overflow-hidden w-[55px]">
+        <img src={blockedPhoto ? blockedPhoto : profile_pic} alt="" />
       </div>
       <div className="flex w-full justify-between">
         <div>
-          <h4 className="font-poppins text-[18px] font-semibold">{name}</h4>
-          <h5 className="text-[14px] font-poppins font-medium text-[#4D4D4DBF]">
-            {time}
-          </h5>
+          <h4 className="font-poppins text-[18px] font-semibold">
+            {blockedName}
+          </h4>
+          
         </div>
         <div className="text-center">
           <p className="md:text-xl text-base font-poppins font-semibold text-white cursor-pointer bg-violet px-5 py-1 rounded-[5px] active:scale-[0.95]">
--- src/components/BlockedUsers/BlockedUsers.jsx
+++ src/components/BlockedUsers/BlockedUsers.jsx
@@ -1,28 +1,62 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import BlockedUserComp from '../BlockedUserComp/BlockedUserComp';
 import { userInfo } from '../../context/UserContext/UserContext';
+import { getAuth } from 'firebase/auth';
+import { getDatabase, onValue, ref } from 'firebase/database';
 
 const BlockedUsers = () => {
   const blockuserDetails = useContext(userInfo)
   const slice=blockuserDetails.slice(0, 5)
+
+    const auth = getAuth();
+    const db = getDatabase();
+    const [blockList, setBlockList] = useState([]);
+    const [noBlock, setNoBlock] = useState(false);
+
+    // for blockList 
+
+     useEffect(() => {
+            onValue(ref(db, "block/"), (snapshot) => {
+            let arr = []
+            
+            snapshot.forEach((block) => {
+             if (
+               auth.currentUser.email===block.val().blockedByEmail
+             ){
+  
+               arr.push(block.val());
+               setNoBlock(true);
+             }
+            });
+            setBlockList(arr);
+            });
+      
+          
+        }, []);
+
   return (
     <div className="p-5 pt-0 rounded-[20px] shadow-custom mt-9 h-[445px] overflow-auto relative ">
       <div className="flex justify-between mb-3 pt-5 bg-white sticky top-[0px] left-0 h-[70px] w-full">
         <h3 className="font-poppins font-semibold text-xl ">Blocked Users</h3>
         <BsThreeDotsVertical className="text-2xl text-violet cursor-pointer " />
       </div>
+      {noBlock ? (
+        <div>
+          {
+          blockList.map((items, idx) => (
+            // console.log(items)
+            <BlockedUserComp key={idx} items={items} blockedName={items.blockedName} blockedPhoto={items.blockedPhoto} />
+          ))
+        }
 
-      <div>
-        {slice.map((items, idx) => (
-          <BlockedUserComp
-          key={idx}
-            image={items.image}
-            name={items.name}
-            time={items.time}
-          />
-        ))}
-      </div>
+        </div>
+      ) : (
+        <p className="font-opnesans text-2xl font-semibold text-center text-darkBlueOne">
+          You haven't blocked anyone
+        </p>
+      )}
+      <div></div>
     </div>
   );
 }
